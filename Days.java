e57b9b0072cbafd2f098398b7e12db0c204963cc
SANJANASRI-84284 <sanjusri962936@gmail.com>
2017-07-20 12:10:34 +0530

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,19 @@
+package Guvi;
+import java.util.*;
+public class Days
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=new String();
+str=s.nextLine();
+if(str.equals("sunday")|| str.equals("sun"))
+{
+	System.out.println("false");
+}
+else
+{
+	System.out.println("true");
+}
+}
+}
